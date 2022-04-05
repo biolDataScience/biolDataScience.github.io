f7224ac5fb74fcbe68f8159dddd1cc8db4662d9c
taddallas <tad.a.dallas@gmail.com>
2022-04-05 15:39:00 -0400

    updates to week 11 and week 12

--- content/code/w11_simulation/analysis.R
+++ content/code/w11_simulation/analysis.R
@@ -8,7 +8,7 @@ plot(mtcars$cyl, mtcars$gear,
   pch=16, las=1, col='dodgerblue')
 dev.off()
 
-cor.test(mtcars$cyl, mtcars$gear)
+cylGear.cor <- cor.test(mtcars$cyl, mtcars$gear)
 
 save.image('analysis.RData')
 
